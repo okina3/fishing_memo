122ca08e0add64380436eb5dadd3d14e3dc9a0dc
okina3 <takosos@icloud.com>
2025-11-27 13:35:19 +0900

    画像のリサイズ機能の実装（interventionImageのインストール）

---
+++ src/app/Providers/InterventionImageServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+
+class InterventionImageServiceProvider extends ServiceProvider
+{
+   /**
+    * ImageManager インスタンスをサービスの登録
+    * @return void
+    */
+   public function register(): void
+   {
+      $this->app->singleton(ImageManager::class, function () {
+         return new ImageManager(new Driver());
+      });
+   }
+
+   /**
+    * ファサードエイリアスの登録
+    * @return void
+    */
+   public function boot(): void
+   {
+      if (! class_exists('InterventionImage')) {
+         if (class_exists('Intervention\\Image\\Facades\\Image', false)) {
+            class_alias('Intervention\\Image\\Facades\\Image', 'InterventionImage');
+         }
+      }
+   }
+}
--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    // 画像のリサイズや加工を行うためのサービスプロバイダー
+    App\Providers\InterventionImageServiceProvider::class,
 ];
