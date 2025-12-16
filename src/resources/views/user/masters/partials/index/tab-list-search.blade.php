5a789adfdd4047e82bb470209c64413998f37a93
okina3 <takosos@icloud.com>
2025-12-16 20:38:24 +0900

    bugfix_レイアウトの細かい調整

--- src/resources/views/user/masters/partials/index/tab-list-search.blade.php
+++ src/resources/views/user/masters/partials/index/tab-list-search.blade.php
@@ -2,7 +2,7 @@
 <div class="mb-5 sm:flex sm:items-end sm:flex-row">
    {{-- タブ表示 --}}
    <div class="mr-10">
-      <p class="mb-2 text-sm text-gray-500">クリックでカテゴリを選択してください。</p>
+      <p class="mb-2 text-sm text-gray-500">クリックでカテゴリを切り替えてください。</p>
       <div class="flex items-center">
          <button type="submit" form="searchForm" name="tab" value="spots"
             class="btn-3 {{ $tab === 'spots' ? 'active' : '' }}" title="釣り場の一覧を表示">
@@ -22,11 +22,14 @@
    <div class=" mt-6 sm:mt-0">
       <p class="mb-2 text-sm text-gray-500">カテゴリごとに検索します。</p>
       <form id="searchForm" method="get" action="{{ route('user.masters.index') }}">
-         <input class="px-2 py-1.5 mr-2 w-48 border rounded" type="text" name="keyword" value="{{ $keyword ?? '' }}"
-            placeholder="検索キーワード">
-         <button class="btn bg-blue-800 hover:bg-blue-700" type="submit" name="tab" value="{{ $tab ?? 'spots' }}">
-            検索
-         </button>
+         <div class="flex gap-2 items-center">
+            <input class="px-2 py-1.5 w-64 border rounded" type="text" name="keyword" value="{{ $keyword ?? '' }}"
+               placeholder="検索キーワード">
+            <button class="btn bg-blue-800 hover:bg-blue-700" type="submit" name="tab"
+               value="{{ $tab ?? 'spots' }}">
+               検索
+            </button>
+         </div>
       </form>
       {{-- エラーメッセージ（検索エリア） --}}
       <x-input-error class="mt-2 bg-white" :messages="$errors->get('keyword')" />
