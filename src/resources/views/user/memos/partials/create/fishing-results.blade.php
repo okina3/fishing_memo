305491097d62bd37eb2f5bec980665ddf8dca81e
okina3 <takosos@icloud.com>
2025-12-20 23:46:43 +0900

    新規メモ作成画面に釣り竿追加機能を実装
    JS、Blade、Controllerを編集し、非同期で釣り竿を追加できるようにしました。

--- src/resources/views/user/memos/partials/create/fishing-results.blade.php
+++ src/resources/views/user/memos/partials/create/fishing-results.blade.php
@@ -2,7 +2,7 @@
    {{-- 釣果の入力 --}}
    <div class="pt-2 flex items-center border-t border-gray-300">
       <h2 class="sub_heading-2">釣果</h2>
-      {{-- 釣り場の追加 --}}
+      {{-- 魚名の追加 --}}
       <x-user.ajax-add.fish-name-add />
    </div>
 
