9f8e759028f98156f45a0278f71e72dfb3b8b6bf
okina3 <takosos@icloud.com>
2025-12-23 14:02:51 +0900

    ペジネーションの修正
    MemoServiceで、返り値を常にペジネーションオブジェクトに変更しました。
    ビューを、ページが複数あるときのみページネーションを表示するように修正しました。

--- src/resources/views/user/memos/index.blade.php
+++ src/resources/views/user/memos/index.blade.php
@@ -8,11 +8,10 @@
          {{-- メモ一覧の表示エリア --}}
          @include('user.memos.partials.index.memos-list')
       </div>
-      {{-- ページネーション --}}
-      @if (method_exists($all_memos, 'links'))
+      {{-- ページネーション（ページが複数あるときのみ表示） --}}
+      @if (isset($all_memos) && $all_memos->hasPages())
          <div class="mt-4">
-            {{ $all_memos->appends(request()->query())->links() }}
-         </div>
+            {{ $all_memos->links() }}
       @endif
    </div>
    <script>
